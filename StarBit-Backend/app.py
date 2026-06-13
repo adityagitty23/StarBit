2edd98e606ae99f5a5f8b52dd1b3b93677d9c1ab
adityagitty23 <aditya3250a@gmail.com>
2026-06-13 23:04:12 +0530

    Northflank deployment

--- StarBit-Backend/app.py
+++ StarBit-Backend/app.py
@@ -509,7 +509,11 @@ def download_qr(
     )
 
 if __name__ == "__main__":
+    port = int(
+        os.environ.get("PORT", 10000)
+    )
+
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=port
     )
